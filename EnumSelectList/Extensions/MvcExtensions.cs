92e06af731448515ce79755362746cb90f478658
NoFlaw <brentonbates@gmail.com>
2014-11-01 01:18:42 -0400

    ...

---
+++ EnumSelectList/Extensions/MvcExtensions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace EnumsAndExtensions.Extensions
+{ 
+    public static class MvcExtensions
+    {
+        private static readonly SelectListItem[] SingleEmptyItem = new[] { new SelectListItem { Text = "", Value = "" } };
+
+        private static Type GetNonNullableModelType(ModelMetadata modelMetadata)
+        {
+            Type realModelType = modelMetadata.ModelType;
+
+            Type underlyingType = Nullable.GetUnderlyingType(realModelType);
+            if (underlyingType != null)
+            {
+                realModelType = underlyingType;
+            }
+            return realModelType;
+        }
+
+        public static String ToDescription(this Enum value)
+        {
+            FieldInfo field = value.GetType().GetField(value.ToString());
+            DisplayAttribute attribute = Attribute.GetCustomAttribute(field, typeof(DisplayAttribute)) as DisplayAttribute;
+            return attribute == null ? value.ToString() : attribute.Name;
+        }
+    
+        public static SelectList ToSelectList<TEnum>(this TEnum enumObj) where TEnum : struct, IComparable, IFormattable, IConvertible
+        {
+            var values = from TEnum e in Enum.GetValues(typeof(TEnum))
+                         select new
+                         {
+                             Id = (int)Enum.Parse(typeof(TEnum), e.ToString(CultureInfo.InvariantCulture)), 
+                             Name = e.ToString(CultureInfo.InvariantCulture)
+                         };
+
+            return new SelectList(values, "Id", "Name", (int)Enum.Parse(typeof(TEnum), enumObj.ToString(CultureInfo.InvariantCulture)));
+        }
+
+        public static String GetEnumDescription<TEnum>(TEnum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if ((attributes != null) && (attributes.Length > 0))
+                return attributes[0].Description;
+            else
+                return value.ToString();
+        }
+
+        public static MvcHtmlString MyEnumDropDownListFor<TModel, TEnum>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TEnum>> expression)
+        {
+            return MyEnumDropDownListFor(htmlHelper, expression, null);
+        }
+
+        public static MvcHtmlString MyEnumDropDownListFor<TModel, TEnum>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TEnum>> expression, object htmlAttributes)
+        {
+            ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+            Type enumType = GetNonNullableModelType(metadata);
+            IEnumerable<TEnum> values = Enum.GetValues(enumType).Cast<TEnum>();
+
+            IEnumerable<SelectListItem> items = from value in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = GetEnumDescription(value),
+                                                    Value = value.ToString(),
+                                                    Selected = value.Equals(metadata.Model)
+                                                };
+
+            // If the enum is nullable, add an 'empty' item to the collection
+            if (metadata.IsNullableValueType)
+                items = SingleEmptyItem.Concat(items);
+
+            return htmlHelper.DropDownListFor(expression, items, htmlAttributes);
+        }
+    }
+}
